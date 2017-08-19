3338dc9d472ffea8c8eb0a858867af0ac686d432
tetete <tetetelego2013@gmail.com>
2017-08-19 17:21:45 +0900

    show other players

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function getcarindex(cid){
 app.post('/setpos', function (request, response) {
     console.log(request.body);
     if(request.body==null)
-        response.send(JSON.stringify(cars);
+        response.send(JSON.stringify(cars));
     if(request.body.rot!=null&&request.body.pos!=null&&request.body.cid!=null){
         var cid=request.body.cid;
         var p=request.body.pos;
