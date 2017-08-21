c439923052c576a8446ca4c7e691de490305cd24
tetete <tetetelego2013@gmail.com>
2017-08-21 20:33:38 +0900

    iPad tilt

--- public/game/game.js
+++ public/game/game.js
@@ -246,6 +246,9 @@ var laptxt="";
 window.addEventListener('devicemotion', function (event) {
     var gv = event.accelerationIncludingGravity;
     tilt=gv.x/4;
+    if(navigator.userAgent.indexOf('iPad') > 0){
+        tilt=-gv.x/4;
+    }
     if(tilt>1)tilt=1;
     if(tilt<-1)tilt=-1;
 });
